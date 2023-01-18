fbf63f2db7e39ae4ccac0e760098438b2aa83f1f
Tsilos Kosbab <38735446+Tsilos1@users.noreply.github.com>
2023-01-17 20:32:15 -0600

    with solutions

---
+++ Bonus.py
@@ -0,0 +1,12 @@
+#Write a function that when given two numbers, finds their greatest common divisor.
+def greatest_common_divisor(a, b):
+    if(b==0):
+        return a
+    else: 
+        # Check out the modulus operator % https://www.w3schools.com/python/python_operators.asp
+        return greatest_common_divisor(b, a%b)
+
+a=15
+b=10
+
+print(greatest_common_divisor(a,b))
--- solution2.py
+++ solution2.py
@@ -1,1 +1,17 @@
 # Write code for algorithm 2 below
+#2. Write a function that prints the natural numbers from 1 to n.
+
+#
+	#your code here
+#
+#output: 1 2 3
+
+def natural_numbers(lowerNum, higherNum):
+    if lowerNum > higherNum:
+        return
+    else:
+        print(lowerNum)
+        natural_numbers(lowerNum + 1, higherNum)
+
+n=10
+natural_numbers(1,n)
